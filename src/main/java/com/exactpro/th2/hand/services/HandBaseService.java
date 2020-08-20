926dbab3f72378a36c900be96df7af82de2b07a2
anna.gaposhkina <anna.gaposhkina@exactprosystems.com>
2020-08-20 11:31:59 +0300

    [JIRA] (TH2-249) Core Hand microservice.

--- src/main/java/com/exactpro/th2/hand/services/HandBaseService.java
+++ src/main/java/com/exactpro/th2/hand/services/HandBaseService.java
@@ -16,74 +16,51 @@
 
 package com.exactpro.th2.hand.services;
 
-import static org.apache.commons.lang3.StringUtils.defaultIfEmpty;
-import static org.apache.commons.lang3.StringUtils.isEmpty;
+import static com.google.protobuf.TextFormat.shortDebugString;
 
 import java.io.IOException;
-import java.util.Map;
+import java.util.List;
+import java.util.stream.Collectors;
 
+import com.exactpro.th2.hand.grpc.rhbatch.RhAction;
+import com.exactpro.th2.hand.grpc.rhbatch.RhActionsList;
+import com.exactpro.th2.hand.grpc.rhbatch.RhBatchGrpc.RhBatchImplBase;
+import com.exactpro.th2.hand.grpc.rhbatch.RhBatchResponse;
+import com.exactpro.th2.hand.grpc.rhbatch.rhactions.RhActionsMessages.*;
+import com.exactpro.th2.hand.grpc.rhbatch.rhactions.RhActionsMessages.Click.ModifiersList;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.exactpro.th2.hand.Config;
 import com.exactpro.th2.hand.IHandService;
-import com.exactpro.th2.hand.grpc.BaseRequest;
-import com.exactpro.th2.hand.grpc.BaseResponse;
-import com.exactpro.th2.hand.grpc.HandBaseGrpc.HandBaseImplBase;
-import com.exactpro.th2.hand.grpc.RhInfo;
-import com.exactprosystems.clearth.connectivity.data.rhdata.RhResponseCode;
-import com.exactprosystems.clearth.connectivity.data.rhdata.RhScriptResult;
 import com.exactprosystems.clearth.connectivity.remotehand.RhClient;
 import com.exactprosystems.clearth.connectivity.remotehand.RhException;
-import com.google.protobuf.Empty;
 
 import io.grpc.stub.StreamObserver;
 
-public class HandBaseService extends HandBaseImplBase implements IHandService
+public class HandBaseService extends RhBatchImplBase implements IHandService
 {
 	private final Logger logger = LoggerFactory.getLogger(getClass());
 	
 	private Config config;
 	private RhClient rhConnection;
 
-	@Override
-	public void getRhInfo(Empty request, StreamObserver<RhInfo> responseObserver) {
-		logger.info("Action: '{}'", "getRhInfo");
-		RhInfo response = RhInfo.newBuilder().setSessionId(rhConnection.getSessionId())
-				.setUserBrowser(rhConnection.getUsedBrowser()).build();
-		responseObserver.onNext(response);
-		responseObserver.onCompleted();
-	}
-
     @Override
-    public void runScript(BaseRequest request, StreamObserver<BaseResponse> responseObserver) {
-        String scriptFile = config.getScriptsDir().resolve(request.getScriptName()).toString();
-        Map<String, String> params = request.getParamsMap();
-        int waitInSeconds = request.getWaitInSeconds();
+    public void executeRhActionsBatch(RhActionsList request, StreamObserver<RhBatchResponse> responseObserver) {
+        logger.info("Action: '{}', request: '{}'", "executeRhActionsBatch", shortDebugString(request));
 
-        logger.info("Action: '{}', Script name: '{}', Parameters:'{}'", "runScript", request.getScriptName(), params);
-        
-        RhScriptResult scriptResult = new RhScriptResult();
-        String errMsg = "";
+        int code = 200;
+        String dataString = null;
         try {
-            scriptResult = rhConnection.executeScript(scriptFile, params, waitInSeconds == 0 ? 10 : waitInSeconds);
+			dataString = rhConnection.send(buildScript(request));
         } 
-        catch (RhException e ) {
-            scriptResult.setCode(RhResponseCode.UNKNOWN.getCode());
-            errMsg = "Error occured while fetching data from Remotehand";
-            logger.warn(errMsg, e);
-        } 
-        catch (IOException e) {
-            scriptResult.setCode(RhResponseCode.UNKNOWN.getCode());
-            errMsg = String.format("Error occured while reading script file '%s'", scriptFile);
-            logger.warn(errMsg, scriptFile, e);
+        catch (RhException | IOException e ) {
+            code = 500; // FIXME RhClient has no public methods returning RhResponse, so code is lost
+            logger.warn("Error occurred while fetching data from Remotehand", e);
         }
-        
-        BaseResponse response = BaseResponse.newBuilder()
-                .setScriptResult(RhResponseCode.byCode(scriptResult.getCode()).toString())
-                .setErrorMessage(isEmpty(errMsg) ? defaultIfEmpty(scriptResult.getErrorMessage(), "") : errMsg)
-                .setSessionId(rhConnection.getSessionId())
-                .addAllTextOut(scriptResult.getTextOutput())
+        RhBatchResponse response = RhBatchResponse.newBuilder()
+                .setCode(code)
+                .setData(dataString)
                 .build();
 
         responseObserver.onNext(response);
@@ -94,5 +71,168 @@ public class HandBaseService extends HandBaseImplBase implements IHandService
 	public void init(Config config, RhClient rhConnection) {
 		this.config = config;
 		this.rhConnection = rhConnection;
+	}
+
+	private String buildScript(RhActionsList actionsList)
+	{
+		StringBuilder sb = new StringBuilder();
+
+		List<RhAction> actionList = actionsList.getRhActionList();
+		for (RhAction action : actionList)
+		{
+			switch (action.getActionCase())
+			{
+				case OPEN:
+					addOpen(sb, action.getOpen());
+					break;
+				case SENDKEYS:
+					addSendKeys(sb, action.getSendKeys());
+					break;
+				case FINDELEMENT:
+					addFindElement(sb, action.getFindElement());
+					break;
+				case CLICK:
+					addClick(sb, action.getClick());
+					break;
+				case SENDKEYSTOACTIVE:
+					addSendKeysToActive(sb, action.getSendKeysToActive());
+					break;
+				case SWITCHWINDOW:
+					addSwitchWindow(sb, action.getSwitchWindow());
+					break;
+			}
+			sb.append(System.getProperty("line.separator"));
+		}
+
+    	return sb.toString();
+	}
+
+	private void appendAndComma(StringBuilder sb, String text) {
+    	if (text != null) {
+			if (text.contains(","))
+				sb.append("\"").append(text).append("\"");
+			else
+				sb.append(text);
+		}
+		sb.append(",");
+	}
+
+	private void addOpen(StringBuilder sb, Open open) {
+    	//#action,#url
+		appendAndComma(sb,"#action");
+		appendAndComma(sb, "#url");
+		sb.append(System.getProperty("line.separator"));
+    	appendAndComma(sb, "Open");
+    	appendAndComma(sb, open.getUrl());
+	}
+
+	private void addSendKeys(StringBuilder sb, SendKeys sendKeys) {
+    	//#action,#wait,#locator,#matcher,#text,#wait2,#locator2,#matcher2,#text2,#canBeDisabled,#clear,#checkInput,#needClick
+		appendAndComma(sb, "#action");
+		appendAndComma(sb, "#wait");
+		appendAndComma(sb, "#locator");
+		appendAndComma(sb, "#matcher");
+		appendAndComma(sb, "#text");
+		appendAndComma(sb, "#wait2");
+		appendAndComma(sb, "#locator2");
+		appendAndComma(sb, "#matcher2");
+		appendAndComma(sb, "#text2");
+		appendAndComma(sb, "#canBeDisabled");
+		appendAndComma(sb, "#clear");
+		appendAndComma(sb, "#checkInput");
+		appendAndComma(sb, "#needClick");
+		sb.append(System.getProperty("line.separator"));
+		appendAndComma(sb, "SendKeys");
+		appendAndComma(sb, String.valueOf(sendKeys.getWait()));
+		appendAndComma(sb, readLocator(sendKeys.getLocator()));
+		appendAndComma(sb, sendKeys.getMatcher());
+		appendAndComma(sb, sendKeys.getText());
+		appendAndComma(sb, String.valueOf(sendKeys.getWait2()));
+		appendAndComma(sb, readLocator(sendKeys.getLocator2()));
+		appendAndComma(sb, sendKeys.getMatcher2());
+		appendAndComma(sb, sendKeys.getText2());
+		appendAndComma(sb, String.valueOf(sendKeys.getCanBeDisabled()));
+		appendAndComma(sb, String.valueOf(sendKeys.getClear()));
+		appendAndComma(sb, String.valueOf(sendKeys.getCheckInput()));
+		appendAndComma(sb, String.valueOf(sendKeys.getNeedClick()));
+	}
+
+	private String readLocator(Locator locator) {
+    	if (locator == null)
+    		return null;
+
+    	switch (locator) {
+			case CSS_SELECTOR:
+				return "cssSelector";
+			case TAG_NAME:
+				return "tagName";
+			case ID:
+				return "id";
+			case XPATH:
+				return "xpath";
+		}
+		return null;
+	}
+
+	private void addFindElement(StringBuilder sb, FindElement findElement) {
+    	// #action,#wait,#locator,#matcher,#id
+		appendAndComma(sb, "#action");
+		appendAndComma(sb, "#wait");
+		appendAndComma(sb, "#locator");
+		appendAndComma(sb, "#matcher");
+		appendAndComma(sb, "#id");
+		sb.append(System.getProperty("line.separator"));
+		appendAndComma(sb, "FindElement");
+		appendAndComma(sb, String.valueOf(findElement.getWait()));
+		appendAndComma(sb, readLocator(findElement.getLocator()));
+		appendAndComma(sb, findElement.getMatcher());
+		appendAndComma(sb, findElement.getId());
+	}
+
+	private void addClick(StringBuilder sb, Click click) {
+		// #action,#wait,#locator,#matcher,#button,#xOffset,#yOffset,#modifiers
+		appendAndComma(sb, "#action");
+		appendAndComma(sb, "#wait");
+		appendAndComma(sb, "#locator");
+		appendAndComma(sb, "#matcher");
+		appendAndComma(sb, "#button");
+		appendAndComma(sb, "#xOffset");
+		appendAndComma(sb, "#yOffset");
+		appendAndComma(sb, "#modifiers");
+		sb.append(System.getProperty("line.separator"));
+		appendAndComma(sb, "Click");
+		appendAndComma(sb, String.valueOf(click.getWait()));
+		appendAndComma(sb, readLocator(click.getLocator()));
+		appendAndComma(sb, click.getMatcher());
+		appendAndComma(sb, click.getButton().name().toLowerCase());
+		appendAndComma(sb, String.valueOf(click.getXOffset()));
+		appendAndComma(sb, String.valueOf(click.getYOffset()));
+		appendAndComma(sb, readModifiers(click.getModifiers()));
+	}
+
+	private String readModifiers(ModifiersList modifiersList) {
+    	return modifiersList.getModifierList().stream()
+				.map(modifier -> modifier.name().toLowerCase())
+				.collect(Collectors.joining(","));
+	}
+
+	private void addSendKeysToActive(StringBuilder sb, SendKeysToActive sendKeysToActive) {
+		// #action,#text,#text2
+		appendAndComma(sb, "#action");
+		appendAndComma(sb, "#text");
+		appendAndComma(sb, "#text2");
+		sb.append(System.getProperty("line.separator"));
+		appendAndComma(sb, "SendKeysToActive");
+		appendAndComma(sb, sendKeysToActive.getText());
+		appendAndComma(sb, sendKeysToActive.getText2());
+	}
+
+	private void addSwitchWindow(StringBuilder sb, SwitchWindow switchWindow) {
+    	// #action,#window
+		appendAndComma(sb, "#action");
+		appendAndComma(sb, "#window");
+		sb.append(System.getProperty("line.separator"));
+		appendAndComma(sb, "SwitchWindow");
+		appendAndComma(sb, String.valueOf(switchWindow.getWindow()));
 	}
 }
